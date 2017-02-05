fcc3ec22e4095d8ed83a496d4a9f17712c96f425
Yurij Karcev <yuka@yandex.ru>
2017-02-05 10:44:18 +0400

    strips "И "
    Удаляет "И ", если пользователь ввёл так. И заодно возможную запятую.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ header("Content-Type: application/json");
 if (empty($_GET["text"])) $_GET["text"] = "ТЫ МОЛОДЕЦ!";
 
 $text = mb_strtoupper($_GET["text"],"UTF-8");
+$text = ltrim($text, ", ");
+if (mb_substr($text, 0, 2, "UTF-8") === "И ") $text = ltrim(mb_substr($text, 2));
 $response = <<<RESP
 {
     "response_type": "in_channel",
